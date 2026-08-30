3ba0f6be5cca83206e36820acd3fd63354afe806
R.WASIF DSZ <rwasifdsz.2006@gmail.com>
2026-08-31 03:56:30 +0500

    feat: replace shared_preferences with get_storage
    Migrate storage dependency to get_storage for improved performance and initialization flow.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
   runApp(const DszSaveChatApp());
 }
 
